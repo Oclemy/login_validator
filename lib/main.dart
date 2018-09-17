215d9f1828225ff8fa31013b6e51b5fa798b0def
Oclemy <oclemmi@gmail.com>
2018-09-17 04:17:44 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:login_validator/login.dart';
+
+//Our app
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mr Login',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+//main function
+void main() => runApp(MyApp());
+
